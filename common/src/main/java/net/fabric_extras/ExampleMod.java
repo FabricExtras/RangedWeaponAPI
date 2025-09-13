877f12aed7e143e2c92a87b235d7a76264192f01
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-13 14:10:22 +0200

    Move code into common scope

--- common/src/main/java/net/fabric_extras/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package net.fabric_extras;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "ranged_weapon_api";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
