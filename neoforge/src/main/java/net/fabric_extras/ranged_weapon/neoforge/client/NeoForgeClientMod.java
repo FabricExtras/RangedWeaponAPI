2b81b47f6ef11b7feef66e031c1a7167af2166a6
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-15 00:39:37 +0200

    Update some paths

--- neoforge/src/main/java/net/fabric_extras/ranged_weapon/neoforge/client/NeoForgeClientMod.java
+++ neoforge/src/main/java/net/fabric_extras/ranged_weapon/neoforge/client/NeoForgeClientMod.java
@@ -1,12 +1,13 @@
 package net.fabric_extras.ranged_weapon.neoforge.client;
 
+import net.fabric_extras.ranged_weapon.RangedWeaponMod;
 import net.fabric_extras.ranged_weapon.client.RangedWeaponAPIClient;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = "ranged_weapon_api", value = Dist.CLIENT)
+@EventBusSubscriber(modid = RangedWeaponMod.ID, value = Dist.CLIENT)
 public class NeoForgeClientMod {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
