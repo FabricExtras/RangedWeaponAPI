141f569eda8a07eb43f7725d4a6c2bd886db006b
marium <mariumbacchus@gmail.com>
2025-12-22 20:13:38 +0100

    Fixed test mod

--- fabric/src/testmod/java/net/testmod/TestMod.java
+++ fabric/src/testmod/java/net/testmod/TestMod.java
@@ -4,9 +4,9 @@ import net.fabric_extras.ranged_weapon.api.CustomBow;
 import net.fabric_extras.ranged_weapon.api.RangedConfig;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.util.Identifier;
 
 public class TestMod implements ModInitializer {
@@ -19,7 +19,7 @@ public class TestMod implements ModInitializer {
         var bow = new CustomBow(
                 new Item.Settings().maxDamage(300),
                 new RangedConfig(9, 1F, 1),
-                Items.GOLD_INGOT
+                ItemTags.REPAIRS_GOLD_ARMOR
         );
         Registry.register(
                 Registries.ITEM,
