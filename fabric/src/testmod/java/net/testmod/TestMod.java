69988671d758d1d33956cb1938f7e9e945d8f419
marium <mariumbacchus@gmail.com>
2025-12-23 17:47:11 +0100

    Fixed use action mixin Added custom crossbow to testmod

--- fabric/src/testmod/java/net/testmod/TestMod.java
+++ fabric/src/testmod/java/net/testmod/TestMod.java
@@ -1,30 +1,46 @@
 package net.testmod;
 
 import net.fabric_extras.ranged_weapon.api.CustomBow;
+import net.fabric_extras.ranged_weapon.api.CustomCrossbow;
 import net.fabric_extras.ranged_weapon.api.RangedConfig;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.util.Identifier;
 
 public class TestMod implements ModInitializer {
     public static final String NAMESPACE = "testmod";
-    /**
-     * Runs the mod initializer.
-     */
+
     @Override
     public void onInitialize() {
+        Identifier customBowId = Identifier.of(NAMESPACE, "custom_longbow");
+        Identifier customCrossbowId = Identifier.of(NAMESPACE, "custom_crossbow");
+        RegistryKey<Item> bowKey = RegistryKey.of(RegistryKeys.ITEM, customBowId);
+        RegistryKey<Item> crossbowKey = RegistryKey.of(RegistryKeys.ITEM, customCrossbowId);
+
+        Item.Settings bowSettings = new Item.Settings()
+                .maxDamage(300)
+                .registryKey(bowKey);
+        Item.Settings crossbowSettings = new Item.Settings()
+                .maxDamage(300)
+                .registryKey(crossbowKey);
+
         var bow = new CustomBow(
-                new Item.Settings().maxDamage(300),
+                bowSettings,
                 new RangedConfig(9, 1F, 1),
                 ItemTags.REPAIRS_GOLD_ARMOR
         );
-        Registry.register(
-                Registries.ITEM,
-                Identifier.of(NAMESPACE, "custom_longbow"),
-                bow
+        var crossbow = new CustomCrossbow(
+                crossbowSettings,
+                new RangedConfig(9, 1F, 1),
+                ItemTags.REPAIRS_GOLD_ARMOR
         );
+
+        Registry.register(Registries.ITEM, bowKey, bow);
+        Registry.register(Registries.ITEM, crossbowKey, crossbow);
     }
 }
